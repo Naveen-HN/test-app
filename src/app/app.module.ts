60eb882d7ba5abfdb4df888991e045c299502f1b
Naveen-HN <hnnaveen93@gmail.com>
2020-04-09 17:07:03 -0400

    Added 2 components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ProfileComponent } from './profile/profile.component';
+import { ProfileDetailComponent } from './profile-detail/profile-detail.component';
 
 @NgModule({
   imports: [BrowserModule],
-  declarations: [AppComponent],
+  declarations: [AppComponent, ProfileComponent, ProfileDetailComponent],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
